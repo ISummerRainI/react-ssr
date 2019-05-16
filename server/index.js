095c5a41712f74b2c07ad1cf4ea7c9fa41721a8d
|SummerRain| <huqingbo@haixue.com>
2019-05-16 11:36:52 +0800

    开发模式调试

--- server/index.js
+++ server/index.js
@@ -3,15 +3,24 @@ const ReactSSR = require('react-dom/server');
 const path = require('path');
 
 const app = express();
-const fs = require('fs');
-const serverEntry = require('../dist/server-entry.js').default;
+const isDev = process.env.NODE_ENV === 'development';
 
-app.use('/public', express.static(path.join(__dirname, '../dist')));
-app.get('*', (req, res, next) => {
+if (!isDev) {
+  const fs = require('fs');
+  const serverEntry = require('../dist/server-entry.js').default;
   const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8');
-  const appString = ReactSSR.renderToString(serverEntry);
-  res.send(template.replace('<!-- app -->', appString));
-});
+
+  app.use('/public', express.static(path.join(__dirname, '../dist')));
+  app.get('*', (req, res, next) => {
+    const appString = ReactSSR.renderToString(serverEntry);
+    res.send(template.replace('<!-- app -->', appString));
+  });
+} else {
+  const devStatic = require('./util/dev-static');
+  devStatic(app);
+}
+
+
 
 app.listen(3001, () => {
   console.log('server is runing at: http://127.0.0.1:3001');
---
+++ server/util/dev-static.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const webpack = require('webpack');
+const MemoryFs = require('memory-fs');
+const ReactSSR = require('react-dom/server');
+const proxy = require('http-proxy-middleware');
+
+const serverConfig = require('../../build/webpack.config.server');
+
+const getTemplate = () => {
+  return new Promise((resolve, reject) => {
+    axios.get('http://127.0.0.1:3000/public/index.html').then(res => {
+      resolve(res.data);
+    }).catch(reject);
+  })
+}
+
+const Module = module.constructor;
+
+const mfs = new MemoryFs();
+const serverCompiler = webpack(serverConfig);
+
+serverCompiler.outputFileSystem = mfs;
+let serverBundle;
+serverCompiler.watch({}, (err, status) => {
+  if (err) throw err;
+  status = status.toJson();
+  status.errors.forEach(err => { console.error(err) });
+  status.warnings.forEach(warn => { console.error(warn) });
+
+  const bundlePath = path.join(
+    serverConfig.output.path,
+    serverConfig.output.filename
+  );
+  const bundle = mfs.readFileSync(bundlePath, 'utf8');
+  const m = new Module();
+  m._compile(bundle, 'server-entry.js');
+  serverBundle = m.exports.default;
+})
+
+module.exports = function (app) {
+  app.use('/public', proxy({
+    target: 'http://127.0.0.1:3000'
+  }));
+  app.get('*', (req, res, next) => {
+    getTemplate().then(template => {
+      const content = ReactSSR.renderToString(serverBundle);
+      res.send(template.replace('<!-- app -->', content));
+    }).catch(next)
+  });
+}
